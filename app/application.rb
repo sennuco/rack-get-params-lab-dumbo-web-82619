21c9c81a635dac292123c0186f7380fa80dfc48e
Theresa Cyrus <cyrustheresa@gmail.com>
2019-09-16 11:14:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,8 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-      elsif 
+      elsif req.path.match(/cart/)
+        if @@cart.size == 0
     else
       resp.write "Path Not Found"
     end
